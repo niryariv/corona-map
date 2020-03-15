404f48bfd6fb46131e56b19865092f460a1afe79
Nir Yariv <niryariv@gmail.com>
2020-03-15 20:48:31 +0200

    initial

---
+++ config.js
@@ -0,0 +1,3 @@
+SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSPAP38AM0UaS1NvfrDOuWd-LF8ZBsXVAuCj27GsAbaWnM8LWUSPrr_SfMjeQG7pgsDge0HGGGhXm7h/pub?output=csv';
+ACCESS_KEY = 'U6rASsnD66pPtU0WmVGB';
+EXPIRE_DAYS = 14;
