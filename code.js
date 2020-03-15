f8c7f5b2bdf5ed54bb8a5e131f4a99d234e96b0a
Nir Yariv <niryariv@gmail.com>
2020-03-15 21:39:06 +0200

    link to source

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key='+ACCESS_
     tileSize: 512,
     zoomOffset: -1,
     minZoom: 1,
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
+    attribution: '<a href="https://github.com/niryariv/corona-map">Simple Corona Map</a>|<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
     crossOrigin: true
 }).addTo(map);
 
