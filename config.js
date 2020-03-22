cc07a343f4aff23d80acbe0023de5cab3db38671
Nir Yariv <niryariv@gmail.com>
2020-03-22 19:54:23 +0200

    try new url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQZyMxHcg-BMlCYnXnuielW-4Qbl5-HksZjIZNT0Pxw5SFcfxM5ZIfEutQMNtAomd637jxFXHbNPOKW/pub?output=csv';
-SHEETS_URL = 'https://www.odata.org.il/dataset/8381af6b-57c7-49d8-bdae-63f92aa45abe/resource/72661658-8806-4c6c-ae0c-d826de129e51/download/output.csv'
+SHEETS_URL = 'https://www.odata.org.il/api/3/action/datastore_search?resource_id=72661658-8806-4c6c-ae0c-d826de129e51'
 ACCESS_KEY = 'U6rASsnD66pPtU0WmVGB';
 DATE_SEPARATOR = '/';
 EXPIRE_DAYS = 14;
