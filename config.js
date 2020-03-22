fff4c2251c7d314c6e0698e9c85312dd6a00c225
Nir Yariv <niryariv@gmail.com>
2020-03-22 21:10:52 +0200

    back to original sheet

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQZyMxHcg-BMlCYnXnuielW-4Qbl5-HksZjIZNT0Pxw5SFcfxM5ZIfEutQMNtAomd637jxFXHbNPOKW/pub?output=csv';
-SHEETS_URL = 'https://www.odata.org.il/api/3/action/datastore_search?resource_id=72661658-8806-4c6c-ae0c-d826de129e51'
+SHEETS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQZyMxHcg-BMlCYnXnuielW-4Qbl5-HksZjIZNT0Pxw5SFcfxM5ZIfEutQMNtAomd637jxFXHbNPOKW/pub?output=csv';
 ACCESS_KEY = 'U6rASsnD66pPtU0WmVGB';
 DATE_SEPARATOR = '/';
 EXPIRE_DAYS = 14;
